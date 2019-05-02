0ebc0b9608085ae66906ed952c4bfcddb01c01dd
jpeter-linux <justinhasanaccount@gmail.com>
2019-05-02 23:34:16 +0200

    apply error handling

--- app/src/main/java/com/example/quantum_dice_mobile/MainActivity.kt
+++ app/src/main/java/com/example/quantum_dice_mobile/MainActivity.kt
@@ -1,73 +1,94 @@
 package com.example.quantum_dice_mobile
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.chaquo.python.PyObject
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import android.widget.Toast
 //python integration
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 
+
+
 class MainActivity : AppCompatActivity() {
-    // constructor(parcel: Parcel) : this() {}
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
-
-
         // get reference to button
         val roll = findViewById<Button>(R.id.button)
+
+            // some code
         // set on-click listener
         roll.setOnClickListener {
             val dice = numDice.text.toString()
             val sides = numSides.text.toString()
             val mod = numMod.text.toString()
-            val xdice = Integer.parseInt(dice)
-            val xsides = Integer.parseInt(sides)
-            val xmod = Integer.parseInt(mod)
 
 
-            // LIST
-            val listRoll = mutableListOf(1)
-            listRoll.removeAt(0)
-            for (i in 1..xdice) {
-                //JAVA RANDOM NUMBER  TODO: Create java logic for degraded network conditions
-                //  val randomGenerator = Random()
-                // val randInt = randomGenerator.nextInt(xsides) + 1
-                //quantum random number
-                //START PYTHON
-                if (! Python.isStarted()) {
-                    Python.start(AndroidPlatform(this))
+                // START THE INPUT VALIDATOR
+            if (sides != "" && dice != "") {
+                //good
+                // translate inputs into INT
+                var xdice = Integer.parseInt(dice)
+                val psides = Integer.parseInt(sides)
+                val xsides = psides.toString()
+                val xmod = Integer.parseInt(mod)
+                // LIST
+                val listRoll = mutableListOf(1)
+                listRoll.removeAt(0)
+                for (i in 1..xdice) {
+                    //JAVA RANDOM NUMBER  TODO: Create java logic for degraded network conditions
+
+
+
+
+                    //quantum random number
+                    //START PYTHON
+                    if (! Python.isStarted()) {
+                        Python.start(AndroidPlatform(this))
+                    }
+                    val py: Python = Python.getInstance()
+                    var rawQ = py.getModule("qengine").callAttr("randint", xsides)
+                    var callQ = rawQ.toString()
+                    var qint = Integer.parseInt(callQ)
+                    //END PYTHON
+                    // append to list
+                    listRoll.add(qint) //TODO: re-include randint
                 }
-                val py: Python = Python.getInstance()
-                var rawQ = py.getModule("qengine").callAttr("randint", sides)
-                var callQ = rawQ.toString()
-                var qint = Integer.parseInt(callQ)
-                //END PYTHON
-            // append to list
-                listRoll.add(qint) //TODO: re-include randint
+                val final = listRoll.sum()+xmod
 
-            }
-
-
-            val final = listRoll.sum()+xmod
-
-            //display
+                //display
                 this.findViewById<TextView>(R.id.displayRoll).apply {
-                text = listRoll.toString()
+                    text = listRoll.toString()
+                }
+                this.findViewById<TextView>(R.id.displayMod).apply {
+                    text = mod
+                    //text = qint.toString()
+                }
+                this.findViewById<TextView>(R.id.displayFinal).apply {
+                    text = final.toString()
+                }
+                    // INPUT ERROR HANDLE
+                            } else {
+                displayRoll.text = "You must enter a value for dice & sides."
             }
-            this.findViewById<TextView>(R.id.displayMod).apply {
-                text = mod
-                //text = qint.toString()
-            }
-            this.findViewById<TextView>(R.id.displayFinal).apply {
-                text = final.toString()
-            }
+
             //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
-        }
-    }}
+            } } }
+
+
+
+
+
+
+
+
 
 
